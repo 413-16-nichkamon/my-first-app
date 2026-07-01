2391bbaa790dab81722910796b1601588ee6edad
413-16-nichkamon <54432@yupparaj.ac.th>
2026-07-01 13:02:23 +0700

    Create  digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital Profile✨")
+
+st.header("แนะนำตัว")
+st.write("นางสาวณิชกมล บัวตรง")
+st.write("ชื่อเล่น แพร")
+st.write("เลขที่ 16 ม.4/13")
+
+st.header("🎯 เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย : **อยากวางแผนธุรกิจเบื้องต้น")
+st.write("สิ่งที่อยากเป็นในอนาคต : นักธุรกิจ (businessperson)")

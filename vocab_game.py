173e3a2d1c86b6af93676e13ef006111ab026cc4
413-16-nichkamon <54432@yupparaj.ac.th>
2026-08-20 09:29:09 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -132,4 +132,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวณิชกมล บัวตรง เลขที่ 16 ม.4/13")
